193623558fc0a5a45b745fc29e0d701a4e9ec532
UnaAceituna <oli.oily@yandex.ru>
2025-08-06 23:55:11 +0300

    added hw 6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -1,19 +1,32 @@
-/*
-  У вас есть массив названий пицц вашего конкурента.
-  Создайте скрипт с циклом, который будет проверять ваш набор названий пицц (массив) 
-  и набор названий пицц конкурента (массив), пицц которых нет у конкурента присвойте в переменную "resultUnique" (массив).
-  Если все ваши пиццы есть у конкурента результатом будет "null" присвойте в переменную "resultNull".
 
-  Скрипт не должен зависеть от регистра, в котором указаны названия пицц у вас и конкурента
-  Воспользуйтесь наборами пицц, что приведены ниже.
+//   У вас есть массив названий пицц вашего конкурента.
+//   Создайте скрипт с циклом, который будет проверять ваш набор названий пицц (массив) 
+//   и набор названий пицц конкурента (массив), пицц которых нет у конкурента присвойте в переменную "resultUnique" (массив).
+//   Если все ваши пиццы есть у конкурента результатом будет "null" присвойте в переменную "resultNull".
 
-  Пиццы:
+//   Скрипт не должен зависеть от регистра, в котором указаны названия пицц у вас и конкурента
+//   Воспользуйтесь наборами пицц, что приведены ниже.
+
+//   Пиццы:
   const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
   const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
-*/
 
-let resultUnique;
-let resultNull;
+  let resultUnique = [];
+  let resultNull;
+
+
+  const myPizzasSpread = [...myPizzasT1, ...myPizzasT2]
+  const myPizzas = [...new Set(myPizzasSpread)]
+
+for (const elPizza of myPizzas) {
+  for (const el of competitorPizzas) {
+     if (el.toLowerCase() == elPizza.toLowerCase()) {
+        resultNull = null
+      }
+    } if (!competitorPizzas.includes(elPizza)) {
+        resultUnique.push(elPizza)
+  }
+}
 
 export { resultNull, resultUnique };
