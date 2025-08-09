51fa7b7e65a1d4dba3c1fc4e485b8db3cb38a755
UnaAceituna <oli.oily@yandex.ru>
2025-08-09 21:13:10 +0300

    added hw 7 solutions

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,9 +5,18 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+
+
+function mergeArrays(...arr) {
+  const array = []
+  for (const el of arr) {
+    array.push(...el)
+  }
+  return array
 }
+console.log(mergeArrays([1,2], [3,4], [5,6], [12,6]))
+
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
@@ -15,8 +24,23 @@ function mergeArrays() {
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
 function devideBy(sentence) {
-  // Ваш код
-}
+  const newSentence = sentence.trim().split(" ")
+  let res = ''
+  
+  for (const el of newSentence) {
+    if (el !== ''){
+      if (newSentence.indexOf(el) == 0){
+    res += el.toLowerCase()
+  }
+   else  {
+    res += "_" + el[0].toUpperCase() + el.slice(1).toLowerCase()
+  }
+}}
+  return res
+} 
+
+console.log(devideBy('I am super engineer'))
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -26,7 +50,16 @@ function devideBy(sentence) {
     - Например fibonacci(8) //21
   */
 function fibonacci(n) {
-  // Ваш код
+  let arr = [0, 1,]
+  if (typeof n !== "number" || n < 0) throw new Error("It won't help you")
+  if (n < 2) {
+    return n;
+  } 
+  for(let i = 2; i <= n; i++) {
+    arr.push(arr[i-1] + arr[i-2])
+  }
+  return arr[n]
 }
 
+// console.log(fibonacci(9))
 export { mergeArrays, fibonacci, devideBy };
--- homework-solutions/js/hw-7/task-2.mjs
+++ homework-solutions/js/hw-7/task-2.mjs
@@ -4,18 +4,37 @@
 */
 
 function isPalindrom(word) {
-  // Ваш код
+  if (typeof word !== 'string' || parseInt(word))
+  return false
+  let palindrome = word.split("").reverse().join("")
+  if ((word.toLowerCase() == palindrome.toLowerCase())) {
+    return true
+  } else
+  return false
 }
+console.log(isPalindrom(""))
 
-/*
- 2. findLongestWords()
- Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра 
- и возвращает слово с наибольшим количеством букв. 
- Если таких слов несколько - возвращает их все.
-*/
+//  2. findLongestWords()
+//  Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра 
+//  и возвращает слово с наибольшим количеством букв. 
+//  Если таких слов несколько - возвращает их все.
+
 
 function findLongestWords(sentence) {
-  // Ваш код
-}
+  if (parseInt(sentence) || (typeof sentence !== 'string') || (sentence.length < 1))
+  return []
 
+  let newSentence = sentence.split(" ")
+  let newArr = Math.max(...newSentence.map(letter => letter.length))
+  let words = []
+
+  for (const el of newSentence) {
+    if (newArr === el.length) {
+      words.push(el)
+    }
+  }
+  return words
+}
+  
+console.log(findLongestWords("The quick rown fox jumps"))
 export { isPalindrom, findLongestWords };
