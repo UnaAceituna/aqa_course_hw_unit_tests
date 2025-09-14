bc5305e5e61833649ce64c6df069459e1af6ebc0
UnaAceituna <oli.oily@yandex.ru>
2025-09-14 17:00:04 +0300

    wonderful generics

---
+++ homework-solutions/js/hw-14/task 2.ts
@@ -0,0 +1,20 @@
+// Напишите функцию, реализующую метод массивов map(сам мэп юзать нельзя, надо написать кастомный:). Функция принимеют на вход массив и колбэк. Используйте дженерик типы. 
+//    Затипизировать надо саму функцию и коллбэк.
+//    Создать реализацию функции map, принимающую массив чисел 1-5, возвращающую новый массив, 
+//    где каждый каждый элемент - это элемент исходного массива умноженный на его индекс
+//    Пример:
+//    map([1,2,3,4,5], callback) => [0,2,6,12,20]
+
+type MapCallback<T, U> = (element: T, index: number, array: T[]) => U
+
+function mapThisArray<T, U> (array: T[], callback: MapCallback<T, U>): U[] {
+    const myArr: U[] = [];
+    for (let i = 0; i < array.length; i++) {
+        myArr.push(callback(array[i], i, array))
+    }
+    return myArr
+}
+
+const arrayOfNumbers = [1,2,3,4,5]
+const customMap = mapThisArray(arrayOfNumbers, (elem, index) => elem * index)
+console.log(customMap)
